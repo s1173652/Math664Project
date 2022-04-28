f7f60814ece0259dc9a837509c7dea6bf55efd5e
Brianna Cirillo <60729550+s1173652@users.noreply.github.com>
2022-04-28 03:06:05 -0400

    Updating graphics

--- EllaBrianna_Scratchwork.R
+++ EllaBrianna_Scratchwork.R
@@ -4,10 +4,12 @@ library(ggplot2)
 library(tidyverse)
 library(MASS)
 library(dplyr)
+#install.packages("leaps")
+library(leaps)
 #load data, print out column names
 hate_crimes<-read.csv("NYPD_Hate_Crimes.csv")
 #replace blank values with "NA"; Arrest Id and Arrest Date each have 860 NA values
-hate_crimes[hate == ""] <- NA 
+hate_crimes[hate_crimes == ""] <- NA 
 #turn Arrest Date into Date class 
 hate_crimes$Arrest.Date<-as.Date(hate_crimes$Arrest.Date,format="%m/%d/%Y")
 #Add another column to signify if arrested or not (arrested=1, not arrested = 0)
@@ -24,7 +26,7 @@ shuffled_hate_crimes<-hate_crimes[sample(1:nrow(hate_crimes)), ]
 #divides into training/test
 train<-shuffled_hate_crimes[1:1000,]
 test<-shuffled_hate_crimes[1001:1296,]
-names(train
+
       
 #running full model 
 model1 <- glm(ArrestStatus ~ .,family=binomial(link='logit'),data=train, maxit=100)
